b1e5257a24d869bf9dc498e7e9afceffb4e2efdb
Adhamh Findlay <adhamh@mono-cvs.ximian.com>
2004-06-11 17:45:33 +0000

    Added CSApplication which is basically a custom implementation of NSApplicationMain. Allows the running of nib-less applications
    svn path=/trunk/cocoa-sharp/; revision=29361

---
+++ sample/SimpleWindow/CSApplication.m
@@ -0,0 +1,30 @@
+//
+//  Cocoa#.m
+//  SimpleWindow
+//
+//  Created by adhamh on Thu Jun 10 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+#import <Cocoa/Cocoa.h>
+#import "CSApplication.h"
+#import "CSControl.h"
+
+@implementation CSApplication
+
++(void) csApplicationMain
+{
+	//This is basically an implementation of NSApplicationMain.  The main difference
+	//is that it does not call loadNibNamed because when creating the bridge, we don't
+	//want to hide behind NIBs and NSApplicationMain.
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc]init];
+	[NSApplication sharedApplication];
+	[NSApp setApplicationIconImage: [NSImage imageNamed: @"iTools.tiff"]];
+	
+	//Here is where custom code is implemented to draw the first window, or whatever
+	CSControl *control = [[CSControl alloc]init];
+	[control displayWindow];
+
+	[NSApp run];
+	[pool release];	
+}
+@end
