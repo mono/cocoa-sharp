b5be4d147917b15d7622d1078ed15d11e25e836b
Adhamh Findlay <adhamh@mono-cvs.ximian.com>
2004-06-12 00:01:53 +0000

    adding unused/experimental CSWindowController
    svn path=/trunk/cocoa-sharp/; revision=29391

---
+++ sample/SimpleWindow/CSWindowController.h
@@ -0,0 +1,17 @@
+//
+//  CSWindowController.h
+//  SimpleWindow
+//
+//  Created by adhamh on Thu Jun 10 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface CSWindowController : NSWindowController {
+
+}
+- (void)windowWillLoad;
+- (void)windowDidLoad;
+@end
---
+++ sample/SimpleWindow/CSWindowController.m
@@ -0,0 +1,26 @@
+//
+//  CSWindowController.m
+//  SimpleWindow
+//
+//  Created by adhamh on Thu Jun 10 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import "CSWindowController.h"
+
+
+@implementation CSWindowController
+
+
+- (void)windowWillLoad
+{
+	NSLog(@"Loading CSWindow");
+}
+
+- (void)windowDidLoad
+{
+	NSLog(@"Loaded CSWindow");
+}
+
+
+@end
