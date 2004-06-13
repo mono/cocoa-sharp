826fbd33d9db48c7ea294c982c4318e7668b07ca
Geoff Norton <grompf@sublimeintervention.com>
2004-06-13 18:55:25 +0000

    Updated to support callbacks with marg_list struct
    svn path=/trunk/cocoa-sharp/; revision=29456

--- test/test-4.c
+++ test/test-4.c
@@ -1,6 +1,60 @@
-#include <stdio.h>
+#import <Cocoa/Cocoa.h>
+#import <objc/objc-class.h>
 
+@interface ArgClass : NSObject {
+}
+@end
+
+@implementation ArgClass
++(void)sampleFunction:(NSString *)val1 stringTwo:(NSString *)val2 {
+	NSLog(@"val1=%s", val1);
+	NSLog(@"val2=%s", val1);
+}
+@end
+void callManagedDelegateWithArgs(char *methodToInvoke, void *(managedDelegate)(char *m, void *args)) {
+	printf("NATIVE: callBackToManagedWithArgs(%s)\n", methodToInvoke);
+	void * list = nil;
+	char *type;
+	NSString *string1 = [NSString stringWithCString:"foo"];
+	NSString *string2 = [NSString stringWithCString:"bar"];
+	int offset;
+	
+	printf("NATIVE: callBackToManagedWithArgs(%s):getClass\n", methodToInvoke);
+	Class class = [[ArgClass init] class];
+	if(class == nil)
+		printf("class is nil\n");
+	printf("NATIVE: callBackToManagedWithArgs(%s):getMethod\n", methodToInvoke);
+	struct objc_method *method = class_getClassMethod(class, @selector(sampleFunction:stringTwo:));
+	if(method == nil)
+		printf("method is null\n");
+	printf("NATIVE: callBackToManagedWithArgs(%s):alloc_margs\n", methodToInvoke);
+	marg_malloc(list, method);
+	printf("NATIVE: callBackToManagedWithArgs(%s):method_getArgumentInfo\n", methodToInvoke);
+	method_getArgumentInfo(method, 0, &type, &offset);
+	printf("OFFSET: %i\n", offset);
+	id receiver = [ArgClass init];
+	printf("NATIVE: callBackToManagedWithArgs(%s):method_setValue\n", methodToInvoke);
+	marg_setValue(list, offset, id, receiver);
+	method_getArgumentInfo(method, 1, &type, &offset);
+	printf("OFFSET: %i\n", offset);
+	printf("NATIVE: callBackToManagedWithArgs(%s):method_setValue\n", methodToInvoke);
+	marg_setValue(list, offset, SEL, @selector(sampleFunction));
+	method_getArgumentInfo(method, 2, &type, &offset);
+	printf("OFFSET: %i\n", offset);
+	printf("NATIVE: callBackToManagedWithArgs(%s):method_setValue\n", methodToInvoke);
+	marg_setValue(list, offset, NSString *, string1);
+	method_getArgumentInfo(method, 3, &type, &offset);
+	printf("OFFSET: %i\n", offset);
+	printf("NATIVE: callBackToManagedWithArgs(%s):method_setValue\n", methodToInvoke);
+	marg_setValue(list, offset, NSString *, string2);
+	
+	NSString *bar = marg_getValue(list, offset, NSString *);
+	printf("%i %s\n", offset, [bar cString]);
+	printf("prearg_size: %i\n", marg_prearg_size);
+	managedDelegate(methodToInvoke, list);
+}
 void callManagedDelegate(char *methodToInvoke, void *(managedDelegate)(char *m)) {
 	printf("NATIVE: callBackToManaged(%s)\n", methodToInvoke);
 	managedDelegate(methodToInvoke);
 }
+
--- test/test-4.cs
+++ test/test-4.cs
@@ -2,12 +2,16 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
+using Apple.Foundation;
 
 delegate void ObjcDelegate(String method);
+delegate void ObjcDelegateWithArgs(String method, IntPtr args);
 
 class MainClass {
 	[DllImport("test-4")]
 	static extern void callManagedDelegate(string method, ObjcDelegate d);
+	[DllImport("test-4")]
+	static extern void callManagedDelegateWithArgs(string method, ObjcDelegateWithArgs d);
 	
 	static void Main(string[] args) {
 		Console.WriteLine("Creating class.");
@@ -20,6 +24,14 @@ class MainClass {
 		callManagedDelegate("increment", d);
 		Console.WriteLine("Calling delegate: print");
 		callManagedDelegate("print", d);
+		Console.WriteLine("Creating delegateWithArgs.");
+		ObjcDelegateWithArgs d2 = new ObjcDelegateWithArgs(c.MethodInvokerWithArgs);
+		Console.WriteLine("Calling delegate: printWithArgs");
+		callManagedDelegateWithArgs("printWithArgs", d2);
+		Console.WriteLine("Calling delegate: incrementWithArgs");
+		callManagedDelegateWithArgs("incrementWithArgs", d2);
+		Console.WriteLine("Calling delegate: printWithArgs");
+		callManagedDelegateWithArgs("printWithArgs", d2);
 	}
 }
 
@@ -30,15 +42,41 @@ public class CoolClass {
 		Console.WriteLine("i={0}", i);
 	}
 
+	public void printWithArgs(IntPtr args) {
+		ArgStruct realArgs = (ArgStruct)Marshal.PtrToStructure(args, typeof(ArgStruct));
+		NSString string11 = new NSString(realArgs.string1);
+		NSString string22 = new NSString(realArgs.string2);
+		NSString string3 = new NSString("footest");
+		Console.WriteLine("i={0}", i);
+		Console.WriteLine("string3={0}", string3.ToString());
+		Console.WriteLine("string22={0}", string22.ToString());
+		Console.WriteLine("string11={0}", string11.ToString());
+	}
 	public void increment() {
+		i++;
+	}
+	public void incrementWithArgs(IntPtr args) {
 		i++;
 	}
 
 	public void MethodInvoker(String method) {
 		this.GetType().InvokeMember(method, BindingFlags.Default | BindingFlags.InvokeMethod, null, this, null);
 	}
+	public void MethodInvokerWithArgs(String method, IntPtr args) {
+		Console.WriteLine("MANAGED: Invoking {0}", method);
+		this.GetType().InvokeMember(method, BindingFlags.Default | BindingFlags.InvokeMethod, null, this, new object[]{args});
+	}
 	public CoolClass()
 	{
 		i = 1;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct ArgStruct {
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst=128)] public byte[] ppcstuff;
+		[MarshalAs(UnmanagedType.U4)] public IntPtr id;
+		[MarshalAs(UnmanagedType.U4)] public IntPtr sel;
+		[MarshalAs(UnmanagedType.U4)] public IntPtr string1;
+		[MarshalAs(UnmanagedType.U4)] public IntPtr string2;
 	}
 }
