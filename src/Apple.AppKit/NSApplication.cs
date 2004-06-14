a12a4ed4c8323bb7092eee2a19efde9e5e3f088d
Geoff Norton <grompf@sublimeintervention.com>
2004-06-14 04:30:51 +0000

    Implemented stopModal.
    svn path=/trunk/cocoa-sharp/; revision=29479

--- src/Apple.AppKit/NSApplication.cs
+++ src/Apple.AppKit/NSApplication.cs
@@ -20,12 +20,20 @@ namespace Apple.AppKit
 		[DllImport("AppKitGlue")]
 		static extern int NSApplication_runModalForWindow(IntPtr THIS, IntPtr theWindow);
 
+		[DllImport("AppKitGlue")]
+		static extern void NSApplication_stopModal(IntPtr THIS);
+
 		private NSApplication() : this(IntPtr.Zero) {}
 		protected internal NSApplication(IntPtr raw) : base(raw) {}
 
 		public static NSApplication sharedApplication()
 		{
 			return new NSApplication(NSApplication__sharedApplication(NSApplication_class));
+		}
+		
+		public static void stopModal()
+		{
+			NSApplication_stopModal(NSApplication__sharedApplication(NSApplication_class));
 		}
 
 		public int runModalForWindow(NSWindow theWindow)
