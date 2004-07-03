6bc1d4c40f7ee5b2569d735988f8155e1c016a47
Geoff Norton <grompf@sublimeintervention.com>
2004-07-03 20:06:21 +0000

    Monodoc using nib now
    svn path=/trunk/cocoa-sharp/; revision=30710

--- monodoc/browser.cs
+++ monodoc/browser.cs
@@ -79,168 +79,28 @@ class Driver {
 		}
 		Settings.RunningGUI = true;
 		Browser browser = new Browser ();
-
-		NSApplication.SharedApplication.run();
+		browser.Run();
 		return 0;
 	}
 }
 
+[ObjCRegister("Controller")]
 class Browser : NSObject {
-	// The window controller	
-	NSWindowController Windowcontroller;
-	// The main window
-	NSWindow Window;
-	// Where we render the contents
-	WebView Webview;
-	WebFrame mainFrame;
-	NSOutlineView ov;
-
-	//
-	// Left-hand side Browsers
-	//
-	string CurrentUrl;
-	
-	internal RootTree help_tree;
-
-	// For the status bar.
-	uint context_id;
-	BrowserController browserController;
-	NSString CaptionID;
+	[ObjCConnect]
+	public NSDrawer drawer;
+	[ObjCConnect]
+	public NSOutlineView outlineView;
 
 	public Browser ()
-	{
-	    const uint NSViewNotSizable                    =  0;
-    	const uint NSViewMinXMargin                    =  1;
-    	const uint NSViewWidthSizable                  =  2;
-    	const uint NSViewMaxXMargin                    =  4;
-    	const uint NSViewMinYMargin                    =  8;
-    	const uint NSViewHeightSizable                 = 16;
-    	const uint NSViewMaxYMargin                    = 32;
-	
-		// Do the monodoc stuff
-		help_tree = RootTree.LoadTree ();
-		browserController = new BrowserController(help_tree);
+	{}
 
-		Window = new NSWindow(new NSRect(200, 180, 600, 480),
-			(uint)(NSWindowMask.NSMiniaturizableWindowMask | NSWindowMask.NSClosableWindowMask 
-			| NSWindowMask.NSTitledWindowMask | NSWindowMask.NSResizableWindowMask),
-			NSBackingStoreType.NSBackingStoreBuffered,
-			false);
-		Window.title = "Monodoc";
-		Windowcontroller = new NSWindowController(Window);
-
-		NSView drawerView = new NSView(new NSRect(0,0,250,480));
-		drawerView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
-
-		NSDrawer drawer = new NSDrawer(new NSSize(250,480), NSRectEdge.NSMaxXEdge);
-		drawer.parentWindow = Window;
-		drawer.contentView = drawerView;
-
-		NSTabView tabView = new NSTabView(new NSRect(new NSPoint(0,0),drawer.contentSize));
-		tabView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
-	
-		drawerView.addSubview(tabView);
-		
-		NSTabViewItem tabViewItem = new NSTabViewItem("Contents");
-		tabViewItem.label = "Contents";
-		tabView.addTabViewItem(tabViewItem);
-		NSScrollView sv = new NSScrollView(tabView.contentRect);
-		sv.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
-		sv.hasVerticalScroller = true;
-		sv.hasHorizontalScroller = true;
-		sv.autohidesScrollers = true;
-		tabViewItem.view = sv;
-
-		ov = new NSOutlineView(sv.documentVisibleRect);
-		ov.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
-		sv.documentView = ov;
-		CaptionID = new NSString("Caption");
-		CaptionID.retain();
-		NSTableColumn c = new NSTableColumn(CaptionID);
-		c.width = sv.contentSize.width;
-		c.editable = false;
-		c.resizable = true;
-		((NSCell)c.headerCell).stringValue = "";
-		ov.addTableColumn(c);
-		ov.outlineTableColumn = c;
-
-		ov.dataSource = browserController;
-		ov.target = this;
-		ov.doubleAction = "selectedItem";
- 		
-		tabViewItem = new NSTabViewItem("Index");
-		tabViewItem.label = "Index";
-		tabView.addTabViewItem(tabViewItem);
-		tabViewItem = new NSTabViewItem("Search");
-		tabViewItem.label = "Search";
-		tabView.addTabViewItem(tabViewItem);
-	
-		NSBundle.BundleWithPath("/System/Library/Frameworks/WebKit.framework").load();
-		Webview = new WebView(new NSRect(0, 0, 600, 480));
-		Webview.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
-		mainFrame = (WebFrame)Webview.mainFrame;
-
-		((NSView)Window.contentView).addSubview(Webview);
-		
-		drawer.open();
-		
-		Windowcontroller.showWindow(NSApplication.SharedApplication);
-	}
-
-	[ObjCExport("selectedItem")]
-	public void DoubleClick() {
-		BrowserItem bi = ov.itemAtRow(ov.selectedRow) as BrowserItem;
-		Console.WriteLine("Going to load {0}", bi);
-		if(bi.node.URL != null)
-		{
-			string url = help_tree.RenderUrl(bi.node.URL, out bi.node);
-			if (url != null)
-				mainFrame.loadHTMLString_baseURL(url, null);
+	public void Run() {
+		if(!NSBundleAppKitExtras.LoadNibNamed_owner("monodoc.nib", NSApplication.SharedApplication)) {
+			Console.WriteLine("Cant load nib");
+			return;
 		}
+		NSApplication.SharedApplication.run();
 	}
-
-/*
-	//
-	// Renders the HTML text in `text' which was computed from `url'.
-	// The Node matching is `matched_node'.
-	//
-	// `url' is only used for debugging purposes
-	//
-	[ObjCExport("doRender:")]
-	public void Render (string text, Node matched_node, string url)
-	{
-		CurrentUrl = url;
-
-		Gtk.HTMLStream stream = html.Begin ("text/html");
-
-		stream.Write ("<html><body>");
-		stream.Write (text);
-		stream.Write ("</body></html>");
-		html.End (stream, HTMLStreamStatus.Ok);
-		if (matched_node != null) {
-			if (tree_browser.SelectedNode != matched_node)
-				tree_browser.ShowNode (matched_node);
-
-			title_label.Text = matched_node.Caption;
-
-			if (matched_node.Nodes != null) {
-				int count = matched_node.Nodes.Count;
-				string term;
-
-				if (count == 1)
-					term = "subpage";
-				else
-					term = "subpages";
-
-				subtitle_label.Text = count + " " + term;
-			} else
-				subtitle_label.Text = "";
-		} else {
-			title_label.Text = "Error";
-			subtitle_label.Text = "";
-		}
-	}
-*/
 }
 
 class BrowserItem : NSObject {
@@ -290,15 +150,24 @@ Console.WriteLine("DEBUG: ~" + this + " Raw={0,8:x}", (int)Raw);
 	}
 }
 
+[ObjCRegister("BrowserDataSource")]
 class BrowserController : NSObject {
 	internal RootTree help_tree;
 	internal IList items = new ArrayList();
+
 
 	public BrowserController(RootTree _tree) {
 		help_tree = _tree;
 		foreach (Node node in help_tree.Nodes)
 			items.Add(new BrowserItem(node));
 Console.WriteLine("DEBUG: " + this + ".ctor Raw={0,8:x}", (int)Raw);
+	}
+
+	public BrowserController(IntPtr raw, bool rel) : base(raw, rel) {
+		help_tree = RootTree.LoadTree ();
+		foreach (Node node in help_tree.Nodes)
+			items.Add(new BrowserItem(node));
+
 	}
 	~ BrowserController () {
 Console.WriteLine("DEBUG: ~" + this + " Raw={0,8:x}", (int)Raw);
