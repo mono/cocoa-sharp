b40acad5e6fab7d82a8015b453e1182c0b4753ff
Urs C. Muff <urs@mono-cvs.ximian.com>
2004-09-07 12:33:39 +0000

    Add comments for macho structures, add target for macho-reader.exe
    svn path=/trunk/cocoa-sharp/; revision=33513

--- generator/MachOFile.cs
+++ generator/MachOFile.cs
@@ -1,5 +1,5 @@
 //
-// $Id: MachOFile.cs,v 1.10 2004/09/05 03:28:25 urs Exp $
+// $Id: MachOFile.cs,v 1.11 2004/09/07 12:33:39 urs Exp $
 //
 
 using System;
@@ -263,18 +263,84 @@ namespace CocoaSharp {
 		}
 	}
 
+	// http://developer.apple.com/documentation/DeveloperTools/Conceptual/MachORuntime/FileStructure/chapter_4_section_6.html#//apple_ref/doc/uid/20001298/load_command
 	public struct load_command {
+		// An integer indicating the type of load command. Table 3-2 lists the valid load command types
 		public uint cmd;
+		// An integer specifying the total size in bytes of the load command data structure. Each load command structure contains a different set of data, depending on
+		// the load command type, so each might have a different size. The size must always be a multiple of 4. This means the cmdsize field must always divide evenly 
+		// by 4. If the load command data does not divide evenly by 4, add bytes containing zeros to the end until it does.
 		public uint cmdsize;
 	}
+	
+	// Commands				Data structures			Purpose
+	//----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	// LC_SEGMENT			segment_command			Defines a segment of this file to be mapped into the address space of the process that loads this file. It also includes 
+	//												all the sections contained by the segment. See “Mach-O File Format Reference”.
+	// LC_SYMTAB			symtab_command			Specifies the symbol table for this file. This information is used by both static and dynamic linkers when linking the 
+	//												file, and also by debuggers to map symbols to the original source code files from which the symbols were generated.
+	// LC_DYSYMTAB			dysymtab_command		Specifies additional symbol table information used by the dynamic linker.
+	// LC_THREAD			thread_command			For an executable file, the LC_UNIXTHREAD command defines the initial thread state of the main thread of the process.
+	// LC_UNIXTHREAD								LC_THREAD is similar to LC_UNIXTHREAD but does not cause the kernel to allocate a stack.
+	// LC_LOAD_DYLIB		dylib_command			Defines the name of a dynamic shared library that this file links against.
+	// LC_ID_DYLIB			dylib_command			Specifies the install name of a dynamic shared library.
+	// LC_PREBOUND_DYLIB	prebound_dylib_command	For a shared library that this executable is linked prebound against, specifies the modules in the shared library that are used.
+	// LC_LOAD_DYLINKER		dylinker_command		Specifies the dynamic linker that the kernel executes to load this file.
+	// LC_ID_DYLINKER		dylinker_command		Identifies this file as a dynamic linker.
+	// LC_ROUTINES			routines_command		Contains the offset of the shared library initialization routine (specified by the linker’s -init option).
+	// LC_TWOLEVEL_HINTS	twolevel_hints_command	Contains the two-level namespace lookup hint table.
+	// LC_SUB_FRAMEWORK		sub_framework_command	Identifies this file as the implementation of a subframework of an umbrella framework. The name of the umbrella 
+	//												framework is stored in the string parameter.
+	// LC_SUB_UMBRELLA		sub_umbrella_command	Specifies a file that is a subumbrella of this umbrella framework.
+	// LC_SUB_LIBRARY		sub_library_command		Identifies this file as the implementation of a sublibrary of an umbrella framework. The name of the umbrella 
+	//												framework is stored in the string parameter. Note that Apple has not defined a supported location for sublibraries.
+	// LC_SUB_CLIENT		sub_client_command		A subframework can explicitly allow another framework or bundle to link against it by including an LC_SUB_CLIENT load 
+	//												command containing the name of the framework or a client name for a bundle.
 
+	// http://developer.apple.com/documentation/DeveloperTools/Conceptual/MachORuntime/FileStructure/chapter_4_section_4.html#//apple_ref/doc/uid/20001298/BAJHHFFF
 	public struct mach_header {
+		// An integer containing a value identifying this file as a Mach-O executable file. Use the constant MH_MAGIC if the file is intended for use on a CPU with the 
+		// same endianness as the computer on which the compiler is running. The constant MH_CIGAM can be used when the byte ordering scheme of the target machine is 
+		// the reverse of the host CPU.
 		public uint magic;
+		// An integer indicating the CPU architecture you intend to use the file on. Appropriate values include:
+		// - CPU_TYPE_POWERPC for PowerPC-architecture CPUs
+		// - CPU_TYPE_I386 for x86-architecture CPUs
 		public int cputype;
+		// An integer specifying the exact model of the CPU. To run on all PowerPC or x86 processors supported by the Mac OS X kernel, this should be set to 
+		// CPU_SUBTYPE_POWERPC_ALL or CPU_SUBTYPE_I386_ALL
 		public int cpusubtype;
+		// An integer indicating the usage and alignment of the file. Valid values for this field include:
+		// - The MH_OBJECT file type is the format used for intermediate object files. It is a very compact format containing all its sections in one segment. The 
+		//   compiler and assembler usually create one MH_OBJECT file for each source code file. By convention, the file name extension for this format is .o.
+		// - The MH_EXECUTE file type is the format used by standard executable programs.
+		// - The MH_BUNDLE file type is the type typically used by code that you load at runtime (typically called bundles or plug-ins). By convention, the file name 
+		//	 extension for this format is .bundle.
+		// - The MH_DYLIB file type is for dynamic shared libraries. It contains some additional tables to support multiple modules. By convention, the file name 
+		//   extension for this format is .dylib, except for the main shared library of a framework, which does not usually have a file name extension.
+		// - The MH_PRELOAD file type is an executable format used for special-purpose programs that are not loaded by the Mac OS X kernel, such as programs burned into 
+		//   programmable ROM chips. Do not confuse this file type with the MH_PREBOUND flag, which is a flag that the static linker sets in the header structure to mark 
+		//   a prebound image.
+		// - The MH_CORE file type is used to store core files, which are traditionally created when a program crashes. Core files store the entire address space of a 
+		//   process at the time it crashed. You can later run gdb on the core file to figure out why the crash occurred.
+		// - The MH_DYLINKER file type is the type of a dynamic linker shared library. This is the type that dyld is constructed from.
 		public uint filetype;
+		// An integer indicating the number of load commands following the header structure
 		public uint ncmds;
+		// An integer indicating the number of bytes occupied by the load commands following the header structure
 		public uint sizeofcmds;
+		// An integer containing a set of bit flags that indicate the state of certain optional features of the Mach-O file format. These are the masks you can use to 
+		// manipulate this field:
+		// - MH_NOUNDEFS—The object file contained no undefined references when it was built.
+		// - MH_INCRLINK—The object file is the output of an incremental link against a base file and cannot be linked again.
+		// - MH_DYLDLINK—The file is input for the dynamic linker and cannot be statically linked again.
+		// - MH_BINDATLOAD—The dynamic linker should bind the undefined references when the file is loaded.
+		// - MH_PREBOUND—The file’s undefined references are prebound.
+		// - MH_SPLIT_SEGS—The file has its read-only and read-write segments split.
+		// - MH_TWOLEVEL—The image is using two-level namespace bindings.
+		// - MH_FORCE_FLAT—The executable is forcing all images to use flat namespace bindings.
+		// - MH_SUBSECTIONS_VIA_SYMBOLS—The sections of the object file can be divided into individual blocks. These blocks are dead-stripped if they are not used by 
+		//   other code. See “Dead-Code Stripping” in Xcode Build System for details.
 		public uint flags;
 	}
 }
--- generator/Section.cs
+++ generator/Section.cs
@@ -1,5 +1,5 @@
 //
-// $Id: Section.cs,v 1.8 2004/09/04 04:18:32 urs Exp $
+// $Id: Section.cs,v 1.9 2004/09/07 12:33:39 urs Exp $
 //
 
 using System;
@@ -67,31 +67,72 @@ namespace CocoaSharp {
 		}
 	}
 
-	[StructLayout(LayoutKind.Explicit)]
+	// http://developer.apple.com/documentation/DeveloperTools/Conceptual/MachORuntime/FileStructure/chapter_4_section_8.html#//apple_ref/doc/uid/20001298/section
+	//
+	// Directly following a segment_command data structure is an array of section data structures, with the exact count determined by the nsects field of the 
+	// segment_command structure.
 	public struct section {
-		[FieldOffset(0)]
+		// A string specifying the name of this section. The value of this field can be any sequence of ASCII characters, although section names defined by Apple 
+		// begin with two underscores and consist of lowercase letters (as in __text and __data). This field is fixed at 16 bytes in length.
 		public byte sectname0,sectname1,sectname2,sectname3,sectname4,sectname5,sectname6,sectname7,
 			sectname8,sectname9,sectname10,sectname11,sectname12,sectname13,sectname14,sectname15;
-		[FieldOffset(16)]
+		// A string specifying the name of the segment that should eventually contain this section. For compactness, intermediate object files—files of type 
+		// MH_OBJECT—contain only one segment, in which all sections are placed. The static linker places each section in the named segment when building the final 
+		// product (any file that is not of type MH_OBJECT).
 		public byte segname0,segname1,segname2,segname3,segname4,segname5,segname6,segname7,
 			segname8,segname9,segname10,segname11,segname12,segname13,segname14,segname15;
-		[FieldOffset(32)]
+		// An integer specifying the virtual memory address of this section.
 		public uint addr;
-		[FieldOffset(36)]
+		// An integer specifying the size in bytes of the virtual memory occupied by this section.
 		public uint size;
-		[FieldOffset(40)]
+		// An integer specifying the offset to this section in the file.
 		public uint offset;
-		[FieldOffset(44)]
+		// An integer specifying the section’s byte alignment. Specify this as a power of two; for example, a section with 8-byte alignment would have an align value 
+		// of 3 (2 to the 3rd power equals 8).
 		public uint align;
-		[FieldOffset(48)]
+		// An integer specifying the file offset of the first relocation entry for this section.
 		public uint reloff;
-		[FieldOffset(52)]
+		// An integer specifying the number of relocation entries located at reloff for this section.
 		public uint nreloc;
-		[FieldOffset(56)]
+		// An integer divided into two parts. The least significant 8 bits contain the section type, while the most significant 24 bits contain a set of flags that 
+		// specify other attributes of the section. These types and flags are primarily used by the static linker and file analysis tools, such as otool, to determine 
+		// how to modify or display the section. These are the possible types:
+		// - S_REGULAR—This section has no particular type. The standard tools create a __TEXT,__text section of this type.
+		// - S_ZEROFILL—Zero-fill-on-demand section—when this section is first read from or written to, each page within is automatically filled with bytes containing 
+		//   zero.
+		// - S_CSTRING_LITERALS—This section contains only constant C strings. The standard tools create a __TEXT,__cstring section of this type.
+		// - S_4BYTE_LITERALS—This section contains only constant values that are 4 bytes long. The standard tools create a __TEXT,__literal4 section of this type.
+		// - S_8BYTE_LITERALS—This section contains only constant values that are 8 bytes long. The standard tools create a __TEXT,__literal8 section of this type.
+		// - S_LITERAL_POINTERS—This section contains only pointers to constant values.
+		// - S_NON_LAZY_SYMBOL_POINTERS—This section contains only non-lazy pointers to symbols. The standard tools create a section of the __DATA,__nl_symbol_ptrs 
+		//   section of this type.
+		// - S_LAZY_SYMBOL_POINTERS—This section contains only lazy pointers to symbols. The standard tools create a __DATA,__la_symbol_ptrs section of this type.
+		// - S_SYMBOL_STUBS——This section contains symbol stubs. The standard tools create __TEXT,__symbol_stub and __TEXT,__picsymbol_stub sections of this type. 
+		//   See “Indirect Addressing” for more information.
+		// - S_MOD_INIT_FUNC_POINTERS—This section contains pointers to module initialization functions. The standard tools create __DATA,__mod_init_func sections of 
+		//   this type.
+		// - S_MOD_TERM_FUNC_POINTERS—This section contains pointers to module termination functions. The standard tools create __DATA,__mod_term_func sections of this
+		//   type.
+		// - S_COALESCED—This section contains symbols that are coalesced by the static linker and possibly the dynamic linker. More than one file may contain coalesced
+		//   definitions of the same symbol without causing multiple-defined-symbol errors.
+		// The following are the possible attributes of a section:
+		// - S_ATTR_PURE_INSTRUCTIONS—This section contains only executable machine instructions. The standard tools set this flag for the sections __TEXT,__text, 
+		//   __TEXT,__symbol_stub, and __TEXT,__picsymbol_stub.
+		// - S_ATTR_NO_TOC—This section contains coalesced symbols that must not be placed in the table of contents (SYMDEF member) of a static archive library.
+		// - S_ATTR_SOME_INSTRUCTIONS—This section contains executable machine instructions and other data.
+		// - S_ATTR_EXT_RELOC—This section contains references that must be relocated. These references refer to data that exists in other files (undefined symbols). 
+		//   To support external relocation, the maximum virtual memory protections of the segment that contains this section must allow both reading and writing.
+		// - S_ATTR_LOC_RELOC—This section contains references that must be relocated. These references refer to data within this file.
+		// - S_ATTR_STRIP_STATIC_SYMS—The static symbols in this section can be stripped if the MH_DYLDLINK flag of the image’s mach_header header structure is set.
+		// - S_ATTR_NO_DEAD_STRIP—This section must not be dead-stripped. See “Dead-Code Stripping” in Xcode Build System for details.
+		// - S_ATTR_LIVE_SUPPORT—This section must not be dead-stripped if they reference code that is live, but the reference is undetectable.
 		public uint flags;
-		[FieldOffset(60)]
+		// An integer reserved for use with certain section types. For symbol pointer sections and symbol stubs sections that refer to indirect symbol table entries, 
+		// this is the index into the indirect table for this section’s entries. The number of entries is based on the section size divided by the size of the symbol 
+		// pointer or stub. Otherwise this field is set to zero.
 		public uint reserved1;
-		[FieldOffset(64)]
+		// For sections of type S_SYMBOL_STUBS, an integer specifying the size (in bytes) of the symbol stub entries contained in the section. Otherwise, this field is 
+		// reserved for future use and should be set to zero.
 		public uint reserved2;
 	}
 }
