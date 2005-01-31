ec92d1b7914d5c763deab3568d9a1bea221804c6
Geoff Norton <grompf@sublimeintervention.com>
2005-01-31 21:52:18 +0000

    Search some additional paths for bundles.
    svn path=/trunk/cocoa-sharp/; revision=39870

--- generator/custom/AppKit/Application.cs
+++ generator/custom/AppKit/Application.cs
@@ -39,12 +39,20 @@ namespace Apple.AppKit
 		public static void LoadFramework (string frameworkName)
 		{
 			NSBundle frmwrkBundle = (NSBundle)NSBundle.BundleWithPath ("/System/Library/Frameworks/" + frameworkName + ".framework");
+			if (frmwrkBundle == null)
+				frmwrkBundle = (NSBundle)NSBundle.BundleWithPath (NSBundle.MainBundle.resourcePath.ToString () + "/" + frameworkName + ".framework");
+			if (frmwrkBundle == null)
+				frmwrkBundle = (NSBundle)NSBundle.BundleWithPath ("./" + frameworkName + ".framework");
+			if (frmwrkBundle == null)
+				throw new Exception ("Couldn't locate framework: " + frameworkName);
 			frmwrkBundle.load ();
 		}
 			
 		public static bool LoadNib (string nibName)
 		{
 			NSApplication sharedApp = (NSApplication)NSApplication.SharedApplication;
+			NSDictionary dict = (NSDictionary)NSDictionary.DictionaryWithObject_forKey(sharedApp, new NSString ("NSOwner"));
+			ObjCMessaging.objc_msgSend(Apple.Foundation.Class.Get("NSBundle"),"loadNibFile:externalNameTable:withZone:",typeof(System.SByte), typeof(System.IntPtr), NSObject.Net2NS (new NSString(nibName)), typeof(System.IntPtr), NSObject.Net2NS (dict), typeof(System.IntPtr), sharedApp.zone);
 			ObjCMessaging.objc_msgSend(Apple.Foundation.Class.Get("NSBundle"),"loadNibNamed:owner:",typeof(System.SByte), typeof(System.IntPtr), NSObject.Net2NS (new NSString(nibName)), typeof(System.IntPtr), NSObject.Net2NS (sharedApp));
 			return true;
 		}
