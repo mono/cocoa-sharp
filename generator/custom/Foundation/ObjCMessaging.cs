b5eb80acced8c4eaff85015a222b55dd3365b3e7
Geoff Norton <grompf@sublimeintervention.com>
2005-09-14 05:22:16 +0000

    2005-09-18  Geoff Norton  <gnorton@customerdna.com>
            * generator/custom/Foundation/ObjCMessaging.cs: Support objc_msgSend_stret set of calls.
            * generator/custom/AppKit/NSView.override: Fix the NSView.bounds call to send the right type.
                                                                                                                                                     
    
    svn path=/trunk/cocoa-sharp/; revision=50018

--- generator/custom/Foundation/ObjCMessaging.cs
+++ generator/custom/Foundation/ObjCMessaging.cs
@@ -16,10 +16,6 @@ namespace Apple.Tools {
 		static Hashtable types = new Hashtable();
 		static void GenerateAssembly(string type) {
 			string[] argstypes = type.Split('_');
-			Type rettype = System.Type.GetType(argstypes[0]);
-			Type[] args = new Type[argstypes.Length-1]; 
-			for (int i = 0; i < argstypes.Length-1; ++i) 
-				args[i] = System.Type.GetType (argstypes[i+1]);
 
 			if (an == null) {
 				an = new AssemblyName();
@@ -31,7 +27,20 @@ namespace Apple.Tools {
 				module = builder.DefineDynamicModule(an.Name);
 
 			TypeBuilder tb = module.DefineType(type, TypeAttributes.Public);
-			tb.DefinePInvokeMethod("objc_msgSend", "libobjc.dylib", MethodAttributes.PinvokeImpl|MethodAttributes.Static|MethodAttributes.Public, CallingConventions.Standard, rettype, args, CallingConvention.Winapi, CharSet.Auto);
+			Type rettype = System.Type.GetType(argstypes[0]);
+			if (rettype.IsValueType && rettype.Namespace != "System") {
+				Type[] args = new Type[argstypes.Length]; 
+				args[0] = System.Type.GetType ("System.IntPtr");
+				for (int i = 0; i < argstypes.Length-1; ++i) 
+					args[i+1] = System.Type.GetType (argstypes[i+1]);
+
+				tb.DefinePInvokeMethod("objc_msgSend_stret", "libobjc.dylib", MethodAttributes.PinvokeImpl|MethodAttributes.Static|MethodAttributes.Public, CallingConventions.Standard, null, args, CallingConvention.Winapi, CharSet.Auto);
+			} else {
+				Type[] args = new Type[argstypes.Length-1]; 
+				for (int i = 0; i < argstypes.Length-1; ++i) 
+					args[i] = System.Type.GetType (argstypes[i+1]);
+				tb.DefinePInvokeMethod("objc_msgSend", "libobjc.dylib", MethodAttributes.PinvokeImpl|MethodAttributes.Static|MethodAttributes.Public, CallingConventions.Standard, rettype, args, CallingConvention.Winapi, CharSet.Auto);
+			}
 			tb.CreateType();
 			types.Add(type, 1);
 		}
@@ -49,10 +58,23 @@ namespace Apple.Tools {
 
 			string type = marshalrettype.ToString() + "_System.IntPtr_System.IntPtr";
 			Type t = TypeResolve(type).GetType(type);
-			object[] realArgs = new object[2];
-			realArgs[0] = receiver;
-			realArgs[1] = sel_registerName(selector);
-			object ret = t.InvokeMember("objc_msgSend", BindingFlags.InvokeMethod|BindingFlags.Public|BindingFlags.Static, null, null, realArgs);
+			object ret;
+			if (rettype.IsValueType && rettype.Namespace != "System") {
+				IntPtr ptr = Marshal.AllocHGlobal (Marshal.SizeOf (rettype));
+				object[] realArgs = new object[3];
+				realArgs[0] = ptr;
+				realArgs[1] = receiver;
+				realArgs[2] = sel_registerName(selector);
+
+				t.InvokeMember("objc_msgSend_stret", BindingFlags.InvokeMethod|BindingFlags.Public|BindingFlags.Static, null, null, realArgs);
+				ret = Marshal.PtrToStructure (ptr, rettype);
+				Marshal.FreeHGlobal (ptr);
+			} else {
+				object[] realArgs = new object[2];
+				realArgs[0] = receiver;
+				realArgs[1] = sel_registerName(selector);
+				ret = t.InvokeMember("objc_msgSend", BindingFlags.InvokeMethod|BindingFlags.Public|BindingFlags.Static, null, null, realArgs);
+			}
 			if (rettype == typeof(string))
 				ret = Marshal.PtrToStringAuto ((IntPtr)ret);
 			return ret;
@@ -69,17 +91,37 @@ namespace Apple.Tools {
 				type.AppendFormat("_{0}", args[i]);
 			}
 			Type t = TypeResolve(type.ToString()).GetType(type.ToString());
-			object[] realArgs = new object[(args.Length/2)+2];
-			realArgs[0] = receiver;
-			realArgs[1] = sel_registerName(selector);
-			for (int i = 0, j = 2; i < args.Length; i+=2, j++) { 
-				if (args[i+1].GetType ().IsEnum)
-					realArgs[j] = Convert.ChangeType (args[i+1], Enum.GetUnderlyingType (args[i+1].GetType ()));
-				else
-			                realArgs[j] = args[i+1];
-			}
+
 			object o = Activator.CreateInstance(t);
-			object ret = t.InvokeMember("objc_msgSend", BindingFlags.InvokeMethod|BindingFlags.Public|BindingFlags.Static, null, o, realArgs);
+			object ret;
+			if (rettype.IsValueType && rettype.Namespace != "System") {
+				IntPtr ptr = Marshal.AllocHGlobal (Marshal.SizeOf (rettype));
+				object[] realArgs = new object[(args.Length/2)+3];
+				realArgs[0] = ptr;
+				realArgs[1] = receiver;
+				realArgs[2] = sel_registerName(selector);
+				for (int i = 0, j = 3; i < args.Length; i+=2, j++) { 
+					if (args[i+1].GetType ().IsEnum)
+						realArgs[j] = Convert.ChangeType (args[i+1], Enum.GetUnderlyingType (args[i+1].GetType ()));
+					else
+				                realArgs[j] = args[i+1];
+				}
+
+				t.InvokeMember("objc_msgSend_stret", BindingFlags.InvokeMethod|BindingFlags.Public|BindingFlags.Static, null, o, realArgs);
+				ret = Marshal.PtrToStructure (ptr, rettype);
+				Marshal.FreeHGlobal (ptr);
+			} else {
+				object[] realArgs = new object[(args.Length/2)+2];
+				realArgs[0] = receiver;
+				realArgs[1] = sel_registerName(selector);
+				for (int i = 0, j = 2; i < args.Length; i+=2, j++) { 
+					if (args[i+1].GetType ().IsEnum)
+						realArgs[j] = Convert.ChangeType (args[i+1], Enum.GetUnderlyingType (args[i+1].GetType ()));
+					else
+				                realArgs[j] = args[i+1];
+				}
+				ret = t.InvokeMember("objc_msgSend", BindingFlags.InvokeMethod|BindingFlags.Public|BindingFlags.Static, null, o, realArgs);
+			}
 			if (rettype == typeof(string))
 				ret = Marshal.PtrToStringAuto ((IntPtr)ret);
 			return ret;
