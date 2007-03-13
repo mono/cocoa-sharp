7f7dd2e85a422144cd32acbb815843984e52ca7d
David Hudson <dhudson@mono-cvs.ximian.com>
2007-03-13 04:59:52 +0000

    fix namespaces for two examples. Ignore build directories and files
    svn path=/trunk/cocoa-sharp/; revision=74155

--- samples/HelloWorld.cs
+++ samples/HelloWorld.cs
@@ -1,28 +1,32 @@
 using System;
 using Cocoa;
 
-[Register ("ApplicationController")]
-public class HelloWorld : Cocoa.Object {
-	[Connect] public TextField textBox1;
+namespace CocoaSharp.Samples {
+	[Register ("ApplicationController")]
+	public class HelloWorld : Cocoa.Object {
 
-	public HelloWorld (IntPtr native_object) : base (native_object) {}
+		[Connect]
+		public TextField textBox1;
 
-	[Export("applicationWillFinishLaunching:")]
-	public void FinishLoading(Notification notification) {
-		textBox1.Value = "Form Loaded";
+		public HelloWorld (IntPtr native_object) : base (native_object) {}
+
+		[Export("applicationWillFinishLaunching:")]
+		public void FinishLoading(Notification notification) {
+			textBox1.Value = "Form Loaded";
+		}
+
+		[Export("buttonClick:")]
+		public void buttonClick(Cocoa.Object sender) {
+			textBox1.Value = "Button Pushed";
+		}
 	}
 
-	[Export("buttonClick:")]
-	public void buttonClick(Cocoa.Object sender) {
-		textBox1.Value = "Button Pushed";
-	}
+	public class WorldRunner {
+		static void Main (string [] args) {
+			Application.Init ();
+			Application.LoadNib ("HelloWorld.nib");
+			Application.SharedApplication.Icon = new Cocoa.Image ("applemono.icns");
+			Application.Run ();
+		}
+    	} 
 }
-
-public class WorldRunner {
-	static void Main (string [] args) {
-		Application.Init ();
-		Application.LoadNib ("HelloWorld.nib");
-		Application.SharedApplication.Icon = new Cocoa.Image ("applemono.icns");
-		Application.Run ();
-	}
-} 
