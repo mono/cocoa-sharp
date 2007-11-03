0f2d6c88fc495964e21f6639720435174ccf4df1
Todd Schavey <schaveyt@mono-cvs.ximian.com>
2007-11-03 06:50:54 +0000

    Added additional functions for menu access
    svn path=/trunk/cocoa-sharp/; revision=88769

--- src/Cocoa/Menu.cs
+++ src/Cocoa/Menu.cs
@@ -15,8 +15,13 @@ namespace Cocoa {
 			Init ("");
 		}
 
+        public Menu(IntPtr native_object) : base(native_object){
+        }
+
 		public MenuItem AddItem (string title, ActionHandler action, string keyEquivalent) {
-			MenuItem item = (MenuItem) Object.FromIntPtr ((IntPtr) ObjCMessaging.objc_msgSend (NativeObject, "addItemWithTitle:action:keyEquivalent:", typeof (IntPtr), 
+			MenuItem item = (MenuItem) Object.FromIntPtr ((IntPtr) ObjCMessaging.objc_msgSend (
+                NativeObject, "addItemWithTitle:action:keyEquivalent:", 
+                typeof (IntPtr), 
 				typeof (IntPtr), new Cocoa.String (title).NativeObject, 
 				typeof (IntPtr), IntPtr.Zero, 
 				typeof (IntPtr), new Cocoa.String (keyEquivalent).NativeObject));
@@ -24,6 +29,7 @@ namespace Cocoa {
 				item.Click += action;
 			return item;
 		}
+		
 
 		public MenuItem AddItem (string title, string action, string keyEquivalent) {
 			return (MenuItem) Object.FromIntPtr ((IntPtr) ObjCMessaging.objc_msgSend (NativeObject, "addItemWithTitle:action:keyEquivalent:", typeof (IntPtr),
@@ -31,6 +37,7 @@ namespace Cocoa {
 				typeof (IntPtr), ObjCMethods.sel_getUid (action),
 				typeof (IntPtr), new Cocoa.String (keyEquivalent).NativeObject));
 		}
+		
 
 		public void AddItem (MenuItem item) {
 			 ObjCMessaging.objc_msgSend (NativeObject, "addItem:", typeof (void), typeof (IntPtr), item.NativeObject);
@@ -39,5 +46,32 @@ namespace Cocoa {
 		private void Init (string title) {
 			NativeObject = (IntPtr) ObjCMessaging.objc_msgSend (NativeObject, "initWithTitle:", typeof (System.IntPtr), typeof (IntPtr), new Cocoa.String (title).NativeObject);
 		}
+
+        public MenuItem ItemWithTag( int tag ){
+            return (MenuItem) Object.FromIntPtr ((IntPtr) ObjCMessaging.objc_msgSend (
+                NativeObject, "itemWithTag:", 
+                typeof (IntPtr),
+                typeof(int), tag));
+        }
+
+        public MenuItem ItemWithTitle( string title ){
+            return (MenuItem) Object.FromIntPtr ((IntPtr) ObjCMessaging.objc_msgSend (
+                NativeObject, "itemWithTitle:", 
+                typeof (IntPtr),
+                typeof(IntPtr), new Cocoa.String(title).NativeObject));
+        }
+
+        public string Title
+        {
+            get
+            {
+                return Object.FromIntPtr((IntPtr)ObjCMessaging.objc_msgSend(NativeObject, "title", typeof(IntPtr))).ToString();
+            }
+            set
+            {
+                ObjCMessaging.objc_msgSend(NativeObject, "setTitle:", typeof(IntPtr), typeof(IntPtr), new Cocoa.String(value).NativeObject);
+            }
+        }
+
 	}
 }
